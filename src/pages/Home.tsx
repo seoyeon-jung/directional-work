53b3e7dd95f7a837f9e3faed5a2d888073602fe5
seoyeon-jung <jungluchia1204@gmail.com>
2024-02-08 18:40:40 +0900

    design: Home UI 구현
    - 게임 시작 버튼, 기록된 게임 보기 버튼 UI를 구현했습니다
    - useNavigate()를 사용해 버튼 클릭 시 해당 페이지로 넘어가도록 구현했습니다

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,23 @@
 import React from "react";
+import { useNavigate } from "react-router";
 
 export default function Home() {
-  return <div>Home</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div className="flex flex-col items-center space-y-4">
+      <button
+        className="bg-blue-500 hover:bg-blue-300 text-white font-bold py-2 px-4 rounded transition duration-300"
+        onClick={() => navigate("/setting")}
+      >
+        게임 시작
+      </button>
+      <button
+        className="bg-blue-500 hover:bg-blue-300 text-white font-bold py-2 px-4 rounded transition duration-300"
+        onClick={() => navigate("/record")}
+      >
+        기록된 게임 보기
+      </button>
+    </div>
+  );
 }
