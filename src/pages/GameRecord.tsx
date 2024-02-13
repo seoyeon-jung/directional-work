461b97ce0108721870729aa2a493957c0649fcf2
seoyeon-jung <jungluchia1204@gmail.com>
2024-02-13 18:58:42 +0900

    feat: game record 구현
    - 기록된 게임의 결과를 구현했습니다
    - 게임 결과와 mark를 클릭한 순서를 나타내고, winner도 나오도록 했습니다

--- src/pages/GameRecord.tsx
+++ src/pages/GameRecord.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { GameRecordInfo } from "../types";
+import { GameRecordInfo, GameMove } from "../types";
 import { useNavigate } from "react-router";
 
 export default function GameRecord() {
@@ -18,9 +18,67 @@ export default function GameRecord() {
       });
   }, []);
 
+  const renderGameRecord = (gameRecord: GameRecordInfo) => {
+    const { settings, moves } = gameRecord;
+    const { boardSize } = settings;
+    const board: string[][] = Array.from({ length: boardSize }, () =>
+      Array.from({ length: boardSize }, () => "")
+    );
+
+    moves.forEach((move: GameMove) => {
+      const mark =
+        move.player === "player1" ? settings.player1Mark : settings.player2Mark;
+      board[move.row][move.col] = `${mark}: ${move.markOrder}`;
+    });
+
+    const renderBoard = board.map((row, rowIndex) => (
+      <div key={rowIndex} className="flex">
+        {row.map((cell, colIndex) => (
+          <div
+            key={colIndex}
+            className="border border-gray-400  w-12 h-12 flex items-center justify-center"
+            style={{
+              color: cell.includes(settings.player1Mark)
+                ? settings.player1MarkColor
+                : settings.player2MarkColor,
+            }}
+          >
+            {cell || "-"}
+          </div>
+        ))}
+      </div>
+    ));
+
+    return (
+      <div
+        key={gameRecord.id}
+        className="max-w-sm border border-gray-200 rounded-lg shadow mb-8 text-center"
+      >
+        <div className="rounded-t-lg w-80 h-80 flex justify-center items-center">
+          <div>{renderBoard}</div>
+        </div>
+        <hr className="border-t border-gray-200 mt-3" />
+        <div className="p-5 bg-gray-700">
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-white">
+            게임 결과: {gameRecord.winner || "무승부"}
+          </h5>
+          <button
+            className="bg-blue-500 hover:bg-blue-300 text-white font-bold py-2 px-4 mb-4 rounded transition duration-300 mt-4 block mx-auto"
+            onClick={() => navigate(`/record/${gameRecord.id}`)}
+          >
+            자세히 보러가기
+          </button>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <div>
-      <h1 className="text-3xl font-bold mb-4">게임 기록</h1>
+      <h1 className="text-3xl text-center font-bold mb-4">
+        게임 기록 전체보기
+      </h1>
+      {gameRecords.map(renderGameRecord)}
     </div>
   );
 }
